67e909888d179ba68e1bedd66fbeb669cbd936b3
Alexander Neumann <aleneum@gmail.com>
2023-11-06 11:07:26 +0100

    mv src to team_red (coding convention)

--- team_red/utils.py
+++ team_red/utils.py
@@ -1,20 +1,19 @@
-'''
+"""
 ===========================================
         Module: Util functions
 ===========================================
-'''
+"""
 import box
 import yaml
-
 from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-from src.prompts_dq import qa_template, fact_checking_template
 from src.llm import build_llm
+from src.prompts_dq import fact_checking_template, qa_template
 
 # Import config vars
-with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
+with open("config/config.yml", "r", encoding="utf8") as ymlfile:
     cfg = box.Box(yaml.safe_load(ymlfile))
 
 
@@ -22,32 +21,40 @@ def set_qa_prompt():
     """
     Prompt template for QA retrieval for each vectorstore
     """
-    prompt = PromptTemplate(template=qa_template,
-                            input_variables=['context', 'question'])
+    prompt = PromptTemplate(
+        template=qa_template,
+        input_variables=["context", "question"],
+    )
     return prompt
+
 
 def set_fact_checking_prompt():
     """
     Prompt template for QA retrieval for each vectorstore
     """
-    prompt = PromptTemplate(template=fact_checking_template,
-                            input_variables=['context', 'question'])
+    prompt = PromptTemplate(
+        template=fact_checking_template,
+        input_variables=["context", "question"],
+    )
     return prompt
 
 
 def build_retrieval_qa(llm, prompt, vectordb):
-    dbqa = RetrievalQA.from_chain_type(llm=llm,
-                                       chain_type='stuff',
-                                       retriever=vectordb.as_retriever(search_kwargs={'k': cfg.VECTOR_COUNT}),
-                                       return_source_documents=cfg.RETURN_SOURCE_DOCUMENTS,
-                                       chain_type_kwargs={'prompt': prompt}
-                                       )
+    dbqa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectordb.as_retriever(search_kwargs={"k": cfg.VECTOR_COUNT}),
+        return_source_documents=cfg.RETURN_SOURCE_DOCUMENTS,
+        chain_type_kwargs={"prompt": prompt},
+    )
     return dbqa
 
 
 def setup_dbqa():
-    embeddings = HuggingFaceEmbeddings(model_name=cfg.MODEL_EMBEDDED_BIN_PATH,
-                                       model_kwargs={'device': cfg.DEVICE})
+    embeddings = HuggingFaceEmbeddings(
+        model_name=cfg.MODEL_EMBEDDED_BIN_PATH,
+        model_kwargs={"device": cfg.DEVICE},
+    )
     vectordb = FAISS.load_local(cfg.DB_FAISS_PATH, embeddings)
     llm = build_llm()
     qa_prompt = set_qa_prompt()
@@ -55,9 +62,12 @@ def setup_dbqa():
 
     return dbqa
 
+
 def setup_dbqa_fact_checking():
-    embeddings = HuggingFaceEmbeddings(model_name=cfg.MODEL_EMBEDDED_BIN_PATH,
-                                       model_kwargs={'device': cfg.DEVICE})
+    embeddings = HuggingFaceEmbeddings(
+        model_name=cfg.MODEL_EMBEDDED_BIN_PATH,
+        model_kwargs={"device": cfg.DEVICE},
+    )
     vectordb = FAISS.load_local(cfg.DB_FAISS_PATH, embeddings)
     llm = build_llm()
     fact_checking_prompt = set_fact_checking_prompt()
