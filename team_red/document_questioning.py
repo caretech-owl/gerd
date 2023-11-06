77107b35bb4aea315a17b8d201e02c9c5ecf785c
Alexander Neumann <aleneum@gmail.com>
2023-11-06 11:07:26 +0100

    mv src to team_red (coding convention)

--- team_red/document_questioning.py
+++ team_red/document_questioning.py
@@ -1,64 +1,69 @@
-import timeit
 import argparse
+import logging
+import timeit
+
 import box
 import yaml
-import logging
 from src.utils import setup_dbqa, setup_dbqa_fact_checking
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.addHandler(logging.NullHandler())
 
 
 # Import config vars
-with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
+with open("config/config.yml", "r", encoding="utf8") as ymlfile:
     cfg = box.Box(yaml.safe_load(ymlfile))
+
 
 def document_questioning() -> None:
     logging.basicConfig(level=logging.DEBUG)
-    _LOGGER = logging.getLogger(__name__)
-    _LOGGER.addHandler(logging.NullHandler())
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input',
-                        type=str,
-                        default='Wie heißt der Patient?',
-                        help='Enter the query to pass into the LLM')
+    parser.add_argument(
+        "--input",
+        type=str,
+        default="Wie heißt der Patient?",
+        help="Enter the query to pass into the LLM",
+    )
     args = parser.parse_args()
 
     # Setup DBQA
     startQA = timeit.default_timer()
     dbqa = setup_dbqa()
-    response = dbqa({'query': args.input})
+    response = dbqa({"query": args.input})
     endQA = timeit.default_timer()
 
     _LOGGER.debug(f'\nAnswer: {response["result"]}')
-    _LOGGER.debug('='*50)
+    _LOGGER.debug("=" * 50)
 
     # Process source documents
-    source_docs = response['source_documents']
+    source_docs = response["source_documents"]
     for i, doc in enumerate(source_docs):
-        _LOGGER.debug(f'\nSource Document {i+1}\n')
-        _LOGGER.debug(f'Source Text: {doc.page_content}')
+        _LOGGER.debug(f"\nSource Document {i+1}\n")
+        _LOGGER.debug(f"Source Text: {doc.page_content}")
         _LOGGER.debug(f'Document Name: {doc.metadata["source"]}')
         _LOGGER.debug(f'Page Number: {doc.metadata.get("page", 1)}\n')
-        _LOGGER.debug('='* 60)
+        _LOGGER.debug("=" * 60)
 
     _LOGGER.debug(f"Time to retrieve response: {endQA - startQA}")
 
     if cfg.FACTCHECKING == True:
         startFactCheck = timeit.default_timer()
         dbqafact = setup_dbqa_fact_checking()
-        response_fact = dbqafact({'query': response["result"]})
+        response_fact = dbqafact({"query": response["result"]})
         endFactCheck = timeit.default_timer()
         _LOGGER.debug("Factcheck:")
         _LOGGER.debug(f'\nAnswer: {response_fact["result"]}')
-        _LOGGER.debug('='*50)
+        _LOGGER.debug("=" * 50)
 
         # Process source documents
-        source_docs = response_fact['source_documents']
+        source_docs = response_fact["source_documents"]
         for i, doc in enumerate(source_docs):
-            _LOGGER.debug(f'\nSource Document {i+1}\n')
-            _LOGGER.debug(f'Source Text: {doc.page_content}')
+            _LOGGER.debug(f"\nSource Document {i+1}\n")
+            _LOGGER.debug(f"Source Text: {doc.page_content}")
             _LOGGER.debug(f'Document Name: {doc.metadata["source"]}')
             _LOGGER.debug(f'Page Number: {doc.metadata.get("page", 1)}\n')
-            _LOGGER.debug('='* 60)
+            _LOGGER.debug("=" * 60)
 
         _LOGGER.debug(f"Time to retrieve fact check: {endFactCheck - startFactCheck}")
-    
+
     logging.shutdown()
---
+++ team_red/llm.py
@@ -0,0 +1,27 @@
+"""
+===========================================
+        Module: Open-source LLM Setup
+===========================================
+"""
+import box
+import yaml
+from dotenv import find_dotenv, load_dotenv
+from langchain.llms import CTransformers
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Import config vars
+with open("config/config.yml", "r", encoding="utf8") as ymlfile:
+    cfg = box.Box(yaml.safe_load(ymlfile))
+
+
+def build_llm():
+    # Local CTransformers model
+    llm = CTransformers(
+        model=cfg.MODEL_BIN_PATH,
+        model_type=cfg.MODEL_TYPE,
+        config={"max_new_tokens": cfg.MAX_NEW_TOKENS, "temperature": cfg.TEMPERATURE},
+    )
+
+    return llm
