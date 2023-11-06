396d833d1f15a392ccd9b19fdf31eb2324486409
Alexander Neumann <aleneum@gmail.com>
2023-11-06 21:26:47 +0100

    add pydantic, rewrite config

---
+++ team_red/config.py
@@ -0,0 +1,66 @@
+from typing import Any, Type, Tuple, Dict
+from pathlib import Path
+from yaml import safe_load
+
+from pydantic.fields import FieldInfo
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
+
+from team_red.models.backend import BackendConfig
+from team_red.models.data import DataConfig
+from team_red.models.features import FeaturesConfig
+from team_red.models.logging import LoggingConfig
+from team_red.models.model import ModelConfig
+
+
+PROJECT_DIR = Path(__file__).parent.parent
+
+
+class YamlConfig(PydanticBaseSettingsSource):
+    def get_field_value(
+        self, field: FieldInfo, field_name: str
+    ) -> Tuple[Any, str, bool]:
+        raise NotImplementedError()
+
+    def prepare_field_value(
+        self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool
+    ) -> Any:
+        raise NotImplementedError()
+
+    def __call__(self) -> Dict[str, Any]:
+        with Path(PROJECT_DIR, "config", "config.yml").open() as f:
+            d: Dict[str, Any] = safe_load(f)
+        return d
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file_encoding="utf-8")
+
+    backend: BackendConfig
+    data: DataConfig
+    features: FeaturesConfig
+    logging: LoggingConfig
+    model: ModelConfig
+    device: str
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            YamlConfig(settings_cls),
+            env_settings,
+            file_secret_settings,
+        )
+
+
+CONFIG = Settings()
