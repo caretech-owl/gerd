5c6d01fd09d8f5f448ed25c2895de7c336de021c
Alexander Neumann <aleneum@gmail.com>
2023-11-07 11:43:54 +0100

    move vector store generation to qa_service

--- team_red/utils.py
+++ team_red/utils.py
@@ -12,29 +12,6 @@ from langchain.vectorstores import FAISS
 
 from .config import CONFIG
 from .llm import build_llm
-from .prompts_dq import fact_checking_template, qa_template
-
-
-def set_qa_prompt() -> PromptTemplate:
-    """
-    Prompt template for QA retrieval for each vectorstore
-    """
-    prompt = PromptTemplate(
-        template=qa_template,
-        input_variables=["context", "question"],
-    )
-    return prompt
-
-
-def set_fact_checking_prompt() -> PromptTemplate:
-    """
-    Prompt template for QA retrieval for each vectorstore
-    """
-    prompt = PromptTemplate(
-        template=fact_checking_template,
-        input_variables=["context", "question"],
-    )
-    return prompt
 
 
 def build_retrieval_qa(  # type: ignore[no-any-unimported]
@@ -51,30 +28,4 @@ def build_retrieval_qa(  # type: ignore[no-any-unimported]
         return_source_documents=CONFIG.features.return_source,
         chain_type_kwargs={"prompt": prompt},
     )
-    return dbqa
-
-
-def setup_dbqa() -> BaseRetrievalQA:
-    embeddings = HuggingFaceEmbeddings(
-        model_name=CONFIG.data.embedding.model,
-        model_kwargs={"device": CONFIG.device},
-    )
-    vectordb = FAISS.load_local(CONFIG.data.embedding.db_path, embeddings)
-    llm = build_llm()
-    qa_prompt = set_qa_prompt()
-    dbqa = build_retrieval_qa(llm, qa_prompt, vectordb)
-
-    return dbqa
-
-
-def setup_dbqa_fact_checking() -> BaseRetrievalQA:
-    embeddings = HuggingFaceEmbeddings(
-        model_name=CONFIG.data.embedding.model,
-        model_kwargs={"device": CONFIG.device},
-    )
-    vectordb = FAISS.load_local(CONFIG.data.embedding.db_path, embeddings)
-    llm = build_llm()
-    fact_checking_prompt = set_fact_checking_prompt()
-    dbqa = build_retrieval_qa(llm, fact_checking_prompt, vectordb)
-
     return dbqa
