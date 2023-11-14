0171fab4ce41dae119550d66e2c0babab10ebbae
Alexander Neumann <aleneum@gmail.com>
2023-11-14 12:39:46 +0100

    disable server backend for now

--- team_red/backends/rest_server.py
+++ team_red/backends/rest_server.py
@@ -1,32 +1,32 @@
-import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
+# import uvicorn
+# from fastapi import FastAPI
+# from pydantic import BaseModel
 
-from team_red.config import CONFIG
-
-
-class QAQuestion(BaseModel):
-    query: str
+# from team_red.config import CONFIG
 
 
-class QAAnswer(BaseModel):
-    status: int
-    error_msg: str = ""
-    answer: str = ""
+# class QAQuestion(BaseModel):
+#     query: str
 
 
-app = FastAPI()
+# class QAAnswer(BaseModel):
+#     status: int
+#     error_msg: str = ""
+#     answer: str = ""
 
 
-@app.post("/api/v1/qa/query", status_code=200, response_model=QAAnswer)
-def qa_query(*, _: QAQuestion) -> QAAnswer:
-    return QAAnswer(status=200, answer="42")
+# app = FastAPI()
 
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "team_red.backend:app",
-        host=CONFIG.server.ip,
-        port=CONFIG.server.port,
-        reload=True,
-    )
+# @app.post("/api/v1/qa/query", status_code=200, response_model=QAAnswer)
+# def qa_query(*, _: QAQuestion) -> QAAnswer:
+#     return QAAnswer(status=200, answer="42")
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "team_red.backend:app",
+#         host=CONFIG.server.ip,
+#         port=CONFIG.server.port,
+#         reload=True,
+#     )
