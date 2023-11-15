f99b1c1dfa41070173fc1a90e28a07f4833b9a05
Alexander Neumann <aleneum@gmail.com>
2023-11-15 21:11:20 +0100

    add test for config loading

---
+++ tests/test_config.py
@@ -0,0 +1,18 @@
+from team_red.config import Settings
+
+
+def test_config() -> None:
+    config = Settings(_env_file="tests/data/.env")
+
+    assert config.gen.model.prompt.path == "tests/data/prompt.txt"
+    assert (
+        config.gen.model.prompt.text
+        == """Erste Zeile
+Zweite Zeile
+Dritte Zeile
+Umlaute: äöü
+Sonderzeichen: |$&?(())
+{variableA}{variableB}
+"""
+    )
+    assert config.gen.model.prompt.parameters == ["variableA", "variableB"]
