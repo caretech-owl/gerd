95f6acd9f375e3c7ba8fc6e3eae983fcf4b24091
Alexander Neumann <aleneum@gmail.com>
2024-01-26 18:21:25 +0100

    add rest server and client

---
+++ team_red/backends/rest_client.py
@@ -0,0 +1,92 @@
+import logging
+from typing import Dict, List
+
+import requests
+from pydantic import TypeAdapter
+
+from team_red.config import CONFIG
+
+from ..transport import (
+    DocumentSource,
+    GenResponse,
+    PromptConfig,
+    QAAnswer,
+    QAFileUpload,
+    QAQuestion,
+    Transport,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class RestClient(Transport):
+    def __init__(self) -> None:
+        super().__init__()
+        self._url = f"http://{CONFIG.server.host}:{CONFIG.server.port}{CONFIG.server.api_prefix}"
+        self.timeout = 10
+
+    def qa_query(self, question: QAQuestion) -> QAAnswer:
+        return QAAnswer.model_validate(
+            requests.post(
+                f"{self._url}/qa/query",
+                data=question.model_dump_json(),
+                timeout=self.timeout,
+            ).json()
+        )
+
+    def db_query(self, question: QAQuestion) -> List[DocumentSource]:
+        request = question.model_dump_json()
+        _LOGGER.debug("db_query - request: %s", request)
+        response = requests.post(
+            f"{self._url}/qa/db_query",
+            data=question.model_dump_json(),
+            timeout=self.timeout,
+        )
+        _LOGGER.debug("db_query - response: %s", response.json())
+        return TypeAdapter(List[DocumentSource]).validate_python(response.json())
+
+    def add_file(self, file: QAFileUpload) -> QAAnswer:
+        return QAAnswer.model_validate(
+            requests.post(
+                f"{self._url}/qa/file",
+                data=file.model_dump_json(),
+                timeout=self.timeout,
+            ).json()
+        )
+
+    def set_gen_prompt(self, config: PromptConfig) -> PromptConfig:
+        return PromptConfig.model_validate(
+            requests.post(
+                f"{self._url}/gen/prompt",
+                data=config.model_dump_json(),
+                timeout=self.timeout,
+            ).json()
+        )
+
+    def get_gen_prompt(self) -> PromptConfig:
+        return PromptConfig.model_validate(
+            requests.get(f"{self._url}/gen/prompt", timeout=self.timeout).json()
+        )
+
+    def set_qa_prompt(self, config: PromptConfig) -> PromptConfig:
+        return PromptConfig.model_validate(
+            requests.post(
+                f"{self._url}/qa/prompt",
+                data=config.model_dump_json(),
+                timeout=self.timeout,
+            ).json()
+        )
+
+    def get_qa_prompt(self) -> PromptConfig:
+        return PromptConfig.model_validate(
+            requests.get(f"{self._url}/qa/prompt", timeout=self.timeout).json()
+        )
+
+    def generate(self, parameters: Dict[str, str]) -> GenResponse:
+        return GenResponse.model_validate(
+            requests.post(
+                f"{self._url}/gen/generate",
+                json=parameters,
+                timeout=self.timeout,
+            ).json()
+        )
--- team_red/backends/rest_server.py
+++ team_red/backends/rest_server.py
@@ -1,32 +1,90 @@
-# import uvicorn
-# from fastapi import FastAPI
-# from pydantic import BaseModel
+import logging
+from typing import Dict, List
 
-# from team_red.config import CONFIG
+import uvicorn
+from fastapi import APIRouter, FastAPI
 
+from team_red.backends.bridge import Bridge
+from team_red.config import CONFIG
 
-# class QAQuestion(BaseModel):
-#     query: str
+from ..transport import (
+    DocumentSource,
+    GenResponse,
+    PromptConfig,
+    QAAnswer,
+    QAFileUpload,
+    QAQuestion,
+    Transport,
+)
 
-
-# class QAAnswer(BaseModel):
-#     status: int
-#     error_msg: str = ""
-#     answer: str = ""
-
-
-# app = FastAPI()
-
-
-# @app.post("/api/v1/qa/query", status_code=200, response_model=QAAnswer)
-# def qa_query(*, _: QAQuestion) -> QAAnswer:
-#     return QAAnswer(status=200, answer="42")
+_LOGGER = logging.getLogger(__name__)
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(
-#         "team_red.backend:app",
-#         host=CONFIG.server.ip,
-#         port=CONFIG.server.port,
-#         reload=True,
-#     )
+class RestServer(Transport):
+    def __init__(self) -> None:
+        super().__init__()
+
+        prefix = CONFIG.server.api_prefix
+        self._bridge = Bridge()
+        self.router = APIRouter()
+        self.router.add_api_route(f"{prefix}/qa/query", self.qa_query, methods=["POST"])
+        self.router.add_api_route(
+            f"{prefix}/qa/db_query", self.db_query, methods=["POST"]
+        )
+        self.router.add_api_route(f"{prefix}/qa/file", self.add_file, methods=["POST"])
+        self.router.add_api_route(
+            f"{prefix}/gen/prompt", self.set_gen_prompt, methods=["POST"]
+        )
+        self.router.add_api_route(
+            f"{prefix}/gen/prompt", self.get_gen_prompt, methods=["GET"]
+        )
+        self.router.add_api_route(
+            f"{prefix}/qa/prompt", self.set_qa_prompt, methods=["POST"]
+        )
+        self.router.add_api_route(
+            f"{prefix}/qa/prompt", self.get_qa_prompt, methods=["GET"]
+        )
+        self.router.add_api_route(
+            f"{prefix}/gen/generate", self.generate, methods=["POST"]
+        )
+
+    def qa_query(self, question: QAQuestion) -> QAAnswer:
+        return self._bridge.qa_query(question)
+
+    def db_query(self, question: QAQuestion) -> List[DocumentSource]:
+        _LOGGER.debug("dq_query - request: %s", question)
+        response = self._bridge.db_query(question)
+        _LOGGER.debug("dq_query - response: %s", response)
+        return self._bridge.db_query(response)
+
+    def add_file(self, file: QAFileUpload) -> QAAnswer:
+        return self._bridge.add_file(file)
+
+    def set_gen_prompt(self, config: PromptConfig) -> PromptConfig:
+        return self._bridge.set_gen_prompt(config)
+
+    def get_gen_prompt(self) -> PromptConfig:
+        return self._bridge.get_gen_prompt()
+
+    def set_qa_prompt(self, config: PromptConfig) -> PromptConfig:
+        return self._bridge.set_qa_prompt(config)
+
+    def get_qa_prompt(self) -> PromptConfig:
+        return self._bridge.get_qa_prompt()
+
+    def generate(self, parameters: Dict[str, str]) -> GenResponse:
+        return self._bridge.generate(parameters)
+
+
+app = FastAPI()
+rest_server = RestServer()
+app.include_router(rest_server.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "team_red.backends.rest_server:app",
+        host=CONFIG.server.host,
+        port=CONFIG.server.port,
+        reload=True,
+    )
