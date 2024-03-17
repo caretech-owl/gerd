9375cdb66df41af0ddb58cfc3ed99cef570b664b
Ashtonville <niklas.debruyn@hotmail.de>
2024-03-17 16:46:41 +0100

    add custom formatter

--- team_red/gen/generation_service.py
+++ team_red/gen/generation_service.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Dict, Optional
+import string
 
 from ctransformers import AutoModelForCausalLM
 
@@ -9,6 +10,28 @@ from team_red.transport import GenResponse
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.addHandler(logging.NullHandler())
+
+class PartialFormatter(string.Formatter):
+    def __init__(self, missing='', bad_fmt='!!'):
+        self.missing, self.bad_fmt = missing, bad_fmt
+
+    def get_field(self, field_name, args, kwargs):
+        try:
+            val = super(PartialFormatter, self).get_field(field_name, args, kwargs)
+            return val
+        except (KeyError, AttributeError):
+            return None, field_name
+
+    def format_field(self, value, spec):
+        if value is None:
+            return self.missing
+        try:
+            return super(PartialFormatter, self).format_field(value, spec)
+        except ValueError:
+            if self.bad_fmt is not None:
+                return self.bad_fmt
+            else:
+                raise
 
 class GenerationService:
     def __init__(self, config: GenerationConfig) -> None:
@@ -30,14 +53,9 @@ class GenerationService:
 
     def get_prompt(self) -> PromptConfig:
         return self._config.model.prompt
-
-    def generate(self, parameters: Dict[str, str]) -> GenResponse:
-        resolved = self._config.model.prompt.text.format(**parameters)
-        _LOGGER.debug(
-            "\n====== Resolved prompt =====\n\n%s\n\n=============================",
-            resolved,
-        )
-        response = self._model(
+    
+    def model_response(self, resolved: str):
+        model_response = self._model(
             resolved,
             stop = "<|im_end|>",
             max_new_tokens = self._config.model.max_new_tokens,
@@ -46,6 +64,31 @@ class GenerationService:
             temperature = self._config.model.temperature,
             repetition_penalty = self._config.model.repetition_penalty,
         )
+        return model_response
+
+    def generate(self, parameters: Dict[str, str]) -> GenResponse:
+        generate_prompt = self._config.model.prompt.text
+        resolved = generate_prompt.format(**parameters)
+        _LOGGER.debug(
+            "\n====== Resolved prompt =====\n\n%s\n\n=============================",
+            resolved,
+        )
+        response = self.model_response(resolved)
+        _LOGGER.debug(
+            "\n====== Response =====\n\n%s\n\n=============================", response
+        )
+        return GenResponse(text=response)
+
+
+    def gen_continue(self, parameters: Dict) -> GenResponse:
+        fmt = PartialFormatter()
+        continue_prompt = self._config.features.continuation.model.prompt.text
+        resolved = fmt.format(continue_prompt, **parameters)
+        _LOGGER.debug(
+            "\n====== Resolved prompt =====\n\n%s\n\n=============================",
+            resolved,
+        )
+        response = self.model_response(resolved)
         _LOGGER.debug(
             "\n====== Response =====\n\n%s\n\n=============================", response
         )
