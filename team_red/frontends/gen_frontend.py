8b2eb86456d2e45b5a6f14ba29c958d87094835b
Ashtonville <niklas.debruyn@hotmail.de>
2024-03-17 16:47:19 +0100

    add sections and parser in frontend

--- team_red/frontends/gen_frontend.py
+++ team_red/frontends/gen_frontend.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Iterable, Tuple
+import re, difflib
 
 import gradio as gr
 
@@ -23,6 +24,15 @@ _field_labels = {
     "patient_address": "Adresse des Patienten",
 }
 
+sections = [
+    "Anrede",
+    "Anamnese",
+    "Diagnose",
+    "Behandlung",
+    "Medikation",
+    "Grußformel"
+]
+
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -41,7 +51,46 @@ def generate(*fields: gr.Textbox) -> str:
             raise gr.Error(msg)
         params[key] = value
     response = TRANSPORTER.generate(params)
-    return response.text
+    return [response.text,
+            response.text,
+            gr.TextArea(label="Dokument", interactive=True),
+            gr.Button("Kontinuiere Dokument", visible=True)]
+
+
+def compare_paragraphs(src_doc, mod_doc):
+    mod_parts = {}
+    src_doc = src_doc.split("\n\n")
+    mod_doc = mod_doc.split("\n\n")
+    for section_order, src_para in zip(sections, src_doc):
+        mod_para = mod_doc[sections.index(section_order)]
+        if src_para != mod_para:
+            mod_parts[section_order] = mod_para
+    return mod_parts
+
+
+def insert_paragraphs(src_doc, new_para):
+    for section_order, mod_para in new_para.items():
+        src_doc = src_doc.replace(src_doc.split("\n\n")[sections.index(section_order)], mod_para)
+    return src_doc
+
+
+def response_parser(response):
+    parsed_response = {}
+    split_response = response.split("\n\n")
+    for paragraph in split_response:
+        for section in sections:
+            if section in paragraph:
+                parsed_response[section] = paragraph
+                break
+    return parsed_response
+
+
+def gen_continue(src_doc, mod_doc):
+    params = compare_paragraphs(src_doc, mod_doc)
+    response = TRANSPORTER.gen_continue(params)
+    parsed_response = response_parser(response.text)
+    updated_document = insert_paragraphs(src_doc, parsed_response)
+    return [updated_document, updated_document]
 
 
 demo = gr.Blocks()
@@ -50,15 +99,18 @@ with demo:
     config = TRANSPORTER.get_gen_prompt()
 
     gr.Markdown("# Entlassbrief generieren")
+    temp_output = gr.State(" ")
 
     fields = []
     for key in config.parameters:
         fields.append(gr.Textbox(key, visible=False))
         fields.append(gr.Textbox(_field_labels.get(key, key), visible=False))
         fields.append(gr.Textbox(label=_field_labels.get(key, key)))
-    output = gr.TextArea(label="Dokument")
+    output = gr.TextArea(label="Dokument", interactive=False)
     submit_button = gr.Button("Generiere Dokument")
-    submit_button.click(fn=generate, inputs=fields, outputs=output)
+    continuation_button = gr.Button("Kontinuiere Dokument", visible=False)
+    submit_button.click(fn=generate, inputs=fields, outputs=[output, temp_output, output, continuation_button])
+    continuation_button.click(fn=gen_continue, inputs=[temp_output, output], outputs=[output, temp_output])
 
 
 if __name__ == "__main__":
