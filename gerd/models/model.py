e47ecabfbc6a124fa9dd0c7f2ad89f382d74d4b8
Alexander Neumann <aleneum@gmail.com>
2024-06-28 13:25:58 +0200

    add custom field serializer for PromptConfig.template

--- gerd/models/model.py
+++ gerd/models/model.py
@@ -3,7 +3,14 @@ from string import Formatter
 from typing import Any, List, Optional
 
 from jinja2 import Environment, FileSystemLoader, Template
-from pydantic import BaseModel, ConfigDict, ValidationError, computed_field
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    FieldSerializationInfo,
+    ValidationError,
+    computed_field,
+    field_serializer,
+)
 
 
 class PromptConfig(BaseModel):
@@ -16,15 +23,24 @@ class PromptConfig(BaseModel):
     def model_post_init(self, __context: Any) -> None:  # noqa: ANN401
         if not self.text and self.path is not None:
             path = Path(self.path)
-            loader = FileSystemLoader(searchpath=str(path.parent))
-            env = Environment(loader=loader, autoescape=True)
             if Path(self.path).exists():
                 with path.open("r", encoding="utf-8") as f:
                     self.text = f.read()
-                    self.template = env.get_template(path.name)
+                    if path.suffix == ".jinja":
+                        loader = FileSystemLoader(searchpath=str(path.parent))
+                        env = Environment(loader=loader, autoescape=True)
+                        self.template = env.get_template(path.name)
             else:
                 msg = f"Prompt text is not set and '{self.path}' does not exist!"
                 raise ValidationError(msg)
+
+    @field_serializer("template")
+    def serialize_template(
+        self, template: Template | None, _info: FieldSerializationInfo
+    ) -> str:
+        if template:
+            return Path(template.filename).read_text()
+        return None
 
     @computed_field  # type: ignore[misc]
     @property
