61b9fa88cad309236852c7f10198013e2b462afb
Alexander Neumann <aleneum@gmail.com>
2024-06-28 13:49:46 +0200

    test prompt config

---
+++ tests/test_prompt_config.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from gerd.models.model import PromptConfig
+
+
+def test_text() -> None:
+    config = PromptConfig(text="Hello, World!")
+    assert config.text == "Hello, World!"
+
+
+def test_path() -> None:
+    config = PromptConfig(path="tests/data/prompt.txt")
+    assert config.text == Path("tests/data/prompt.txt").read_text()
+    assert config.template is None
+
+
+def test_jinja() -> None:
+    config = PromptConfig(path="tests/data/simple_prompt.jinja")
+    assert "Filled Text" in config.template.render(task="Filled Text")
+
+
+def test_text_parameters() -> None:
+    config = PromptConfig(text="A {value} prompt {test}.")
+    assert config.parameters == ["value", "test"]
+
+
+def test_jinja_parameters() -> None:
+    config = PromptConfig(text="A {{ value }} prompt {{ test }}")
+    assert config.parameters == ["value", "test"]
