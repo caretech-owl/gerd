bd1c969d933fcfb03a001697e47a64cf7cf52130
Alexander Neumann <aleneum@gmail.com>
2024-12-19 16:52:14 +0100

    refactor: rename unstructured examples

--- examples/run_lora_unstructured.py
+++ examples/run_lora_unstructured.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from gerd.config import PROJECT_DIR, load_gen_config
 from gerd.gen.chat_service import ChatService
+from gerd.training.lora import load_training_config
 
 if Path().cwd() != PROJECT_DIR:
     msg = "This example must be run from the project root."
@@ -15,7 +16,7 @@ res = chat.generate({"text": "Sehr geehrte Frau Kollegin,"})
 print(res.text)  # noqa: T201
 
 print("\n\nMit LoRA\n========")  # noqa: T201
-config.model.loras.append(("models/lora"))
+config.model.loras.append(load_training_config("lora_unstructured_example").output_dir)
 chat = ChatService(config)
 res = chat.generate({"text": "Sehr geehrte Frau Kollegin,"})
 print(res.text)  # noqa: T201
