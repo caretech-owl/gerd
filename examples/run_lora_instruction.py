94a87b2b9053de2a872640720b64c90741677f3b
Alexander Neumann <aleneum@gmail.com>
2024-12-19 17:01:36 +0100

    feat: slighty more robust tests

--- examples/run_lora_instruction.py
+++ examples/run_lora_instruction.py
@@ -8,6 +8,15 @@ if Path().cwd() != PROJECT_DIR:
     msg = "This example must be run from the project root."
     raise AssertionError(msg)
 
+lora_config = load_training_config("lora_instruct_example")
+
+if not lora_config.output_dir.exists():
+    msg = (
+        f"LoRA config not found at {lora_config.output_dir}. "
+        "Make sure to run 'train_lora_instruction.py' first."
+    )
+    raise FileNotFoundError(msg)
+
 config = load_gen_config("chat_llama_3_2_1b_instruct")
 
 print("Ohne LoRA\n=========")  # noqa: T201
@@ -16,7 +25,7 @@ res = chat.submit_user_message({"content": "Bitte erweitere die Zahl 42."})
 print(res.text)  # noqa: T201
 
 print("\n\nMit LoRA\n========")  # noqa: T201
-config.model.loras.append(load_training_config("lora_instruct_example").output_dir)
+config.model.loras.append(lora_config.output_dir)
 chat = ChatService(config)
 res = chat.submit_user_message({"content": "Bitte erweitere die Zahl 42."})
 print(res.text)  # noqa: T201
