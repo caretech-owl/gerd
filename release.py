b8ce0d32ab29507573e6655be5904ce48dc96fa3
Alexander Neumann <aleneum@gmail.com>
2025-01-14 18:44:13 +0100

    feat: add uv steps

--- release.py
+++ release.py
@@ -1,18 +1,31 @@
 import logging
+import subprocess
 
 from git import Remote, Repo
 
 from gerd import __version__
 
-_LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+_LOGGER = logging.getLogger("gerd.release")
 
 repo = Repo()
 if repo.is_dirty() or repo.untracked_files:
     msg = "Repository is dirty, please commit or stash your changes"
     raise ValueError(msg)
 try:
+    _LOGGER.info("Sync project...")
+    subprocess.check_call(["uv", "sync"])  # noqa: S603, S607
+    _LOGGER.info("Linting...")
+    subprocess.check_call(["uv", "run", "poe", "lint"])  # noqa: S603, S607
+    _LOGGER.info("Testing...")
+    subprocess.check_call(["uv", "run", "poe", "test"])  # noqa: S603, S607
     origin: Remote = repo.remote("origin")
-    repo.create_tag(f"v{__version__}")
+    tag = f"v{__version__}"
+    _LOGGER.info("Create tag %s...", tag)
+    repo.create_tag(tag)
+    _LOGGER.info("Push tag %s to origin...", tag)
     origin.push(tags=True)
 except ValueError as e:
-    _LOGGER.error("Errro: %s", e)
+    _LOGGER.error("Error: %s", e)
+_LOGGER.info("Done!")
